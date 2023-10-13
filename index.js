ce9399927430b87a7910bbdf84b9f8c5c71cf0a4
ShubhiMiradwalNEU <miradwal.s@northeastern.edu>
2023-10-13 14:37:54 -0400

    updated availabilityzone related code

--- index.js
+++ index.js
@@ -2,12 +2,25 @@
 const pulumi = require("@pulumi/pulumi");
 const aws = require("@pulumi/aws");
 
+const config = new pulumi.Config();
+
+const vpcCidrBlockk = config.require("vpcCidrBlock");
+const publicSubnetCidrBase = config.require("publicSubnetCidrBase");
+const privateSubnetCidrBase = config.require("privateSubnetCidrBase");
+const subnetCidrOffset = config.require("subnetCidrOffset");
+const destinationCidrBlock = config.require("destinationCidrBlock");
+const AWS_REGION = config.require("AWS_REGION");
+
+
+const createResource= async()=>
+{
 const vpc = new aws.ec2.Vpc("main", {
-    cidrBlock: "10.0.0.0/16",
+    cidrBlock: vpcCidrBlockk,
     instanceTenancy: "default",
     tags: {
         Name: "main",
     },
+    region: AWS_REGION,
 });
 
 const internetGateway = new aws.ec2.InternetGateway("gw", {
@@ -16,70 +29,85 @@ const internetGateway = new aws.ec2.InternetGateway("gw", {
         Name: "main",
     },
 });
-const az = aws.getAvailabilityZones({ state: "available" });
 
 const publicSubnets = [];
 const privateSubnets = [];
 let publicRouteTable;
-let publicRouteTableAssociation; 
 let privateRouteTable;
-let privateRouteTableAssociation; 
+let publicRouteTableAssociation;
+let privateRouteTableAssociation;
 
-for (let i = 0; i < 3; i++) {
-    const publicSubnet = new aws.ec2.Subnet(`public-subnet-${i}`, {
+
+publicRouteTable = new aws.ec2.RouteTable("public-rt", {
+    vpcId: vpc.id,
+    tags: {
+        Name: "public-rt",
+    },
+});
+
+privateRouteTable = new aws.ec2.RouteTable("private-rt", {
+    vpcId: vpc.id,
+    tags: {
+        Name: "private-rt",
+    },
+});
+ 
+const findAvailabilityZone = async (AWS_REGION) => {
+    const az = await aws.getAvailabilityZones({ state: "available" });
+    return az.names;
+  }
+  
+async function numberofaz() {
+    const azNames = await findAvailabilityZone(AWS_REGION);
+    console.log(azNames)
+    return azNames.length;
+  }
+
+  (async () => {
+    const numberofAZ = await numberofaz(); 
+    const azNames = await findAvailabilityZone(AWS_REGION);
+    console.log("Number of Availability Zones:", numberofAZ);
+
+
+for (let i = 0; i < Math.min(3,numberofAZ); i++) {
+    console.log("*************");
+
+    const publicSubnet = new aws.ec2.Subnet(`publicsubnet-${i}`, {
         vpcId: vpc.id,
-        cidrBlock: `10.0.${i}.0/24`,
-        availabilityZone: az[i],
+        cidrBlock: `${publicSubnetCidrBase}${i}.0/24`,
+        availabilityZone: azNames[i],
         mapPublicIpOnLaunch: true,
     });
     publicSubnets.push(publicSubnet);
+    console.log(publicSubnets);
 
-    publicRouteTable = new aws.ec2.RouteTable(`public-rt-${i}`, {
-        vpcId: vpc.id,
-        tags: {
-            Name: `public-rt-${i}`,
-        },
+    const publicRoute = new aws.ec2.Route(`publicroute-${i}`, {
+        routeTableId: publicRouteTable.id,
+        destinationCidrBlock: destinationCidrBlock,
+        gatewayId: internetGateway.id,
     });
 
-    publicRouteTableAssociation = new aws.ec2.RouteTableAssociation(`public-rt-assoc-${i}`, {
+    const privateSubnet = new aws.ec2.Subnet(`privatesubnet-${i}`, {
+        vpcId: vpc.id,
+        cidrBlock: `${privateSubnetCidrBase}${i + 3}.0/24`,
+        availabilityZone: azNames[i],
+    });
+    privateSubnets.push(privateSubnet);
+    console.log(privateSubnets);
+
+    new aws.ec2.RouteTableAssociation(`public-rt-${i}`, {
         subnetId: publicSubnet.id,
         routeTableId: publicRouteTable.id,
     });
 
-    const publicRoute = new aws.ec2.Route(`public-route-${i}`, {
-        routeTableId: publicRouteTable.id,
-        destinationCidrBlock: "0.0.0.0/0",
-        gatewayId: internetGateway.id,
-    });
-
-    const privateSubnet = new aws.ec2.Subnet(`private-subnet-${i}`, {
-        vpcId: vpc.id,
-        cidrBlock: `10.0.${i + 3}.0/24`,
-        availabilityZone: az[i],
-    });
-    privateSubnets.push(privateSubnet);
-
-
-    privateRouteTable = new aws.ec2.RouteTable(`private-rt-${i}`, {
-        vpcId: vpc.id,
-        tags: {
-            Name: `public-rt-${i}`,
-        },
-    });
-
-    privateRouteTableAssociation = new aws.ec2.RouteTableAssociation(`private-rt-assoc-${i}`, {
+    new aws.ec2.RouteTableAssociation(`private-rt-${i}`, {
         subnetId: privateSubnet.id,
         routeTableId: privateRouteTable.id,
     });
-
 }
+})();
 
 exports.vpcId = vpc.id;
-exports.publicSubnetIds = publicSubnets.map(subnet => subnet.id);
-exports.privateSubnetIds = privateSubnets.map(subnet => subnet.id);
-exports.publicRouteTableId = publicRouteTable.id;
-exports.publicRouteTableAssociationIds = publicRouteTableAssociation.id;
+}
 
-
-exports.privateRouteTableId = privateRouteTable.id;
-exports.privateRouteTableAssociationIds = privateRouteTableAssociation.id;
+const vpcId = createResource();
